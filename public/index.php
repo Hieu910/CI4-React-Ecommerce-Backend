837f82968e052e457c384e095786ac78397979f2
Hieu <handsomebanana910@gmail.com>
2026-01-16 17:34:16 +0700

    update

--- public/index.php
+++ public/index.php
@@ -3,8 +3,22 @@
 use CodeIgniter\Boot;
 use Config\Paths;
 
-// $origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
-header("Access-Control-Allow-Origin: *");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+
+$allowed_origins = [
+    'https://ci-4-react-ecommerce-frontend.vercel.app',
+    'https://ci-4-react-ecommerce-frontend-git-main-hieu910s-projects.vercel.app/',
+    'https://ci-4-react-ecommerce-frontend-5zmmwh193-hieu910s-projects.vercel.app/',
+
+    'http://localhost:5173',
+    'http://localhost:3000'
+];
+
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
+
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, ngrok-skip-browser-warning, Accept, Origin");
