852283052a4932807951c9a366200ef0a6419783
Hieu <handsomebanana910@gmail.com>
2026-01-17 23:53:41 +0700

    update

--- public/index.php
+++ public/index.php
@@ -3,24 +3,24 @@
 use CodeIgniter\Boot;
 use Config\Paths;
 
-// $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 
-// $allowed_origins = [
-//     'https://ci-4-react-ecommerce-frontend.vercel.app',
-//     'https://ci-4-react-ecommerce-frontend-git-main-hieu910s-projects.vercel.app/',
-//     'https://ci-4-react-ecommerce-frontend-5zmmwh193-hieu910s-projects.vercel.app/',
-//     'http://localhost:5173',
-//     'http://localhost:3000'
-// ];
+$allowed_origins = [
+    'https://ci-4-react-ecommerce-frontend.vercel.app',
+    'https://ci-4-react-ecommerce-frontend-git-main-hieu910s-projects.vercel.app/',
+    'https://ci-4-react-ecommerce-frontend-5zmmwh193-hieu910s-projects.vercel.app/',
+    'http://localhost:5173',
+    'http://localhost:3000'
+];
 
-// if (in_array($origin, $allowed_origins)) {
-//     header("Access-Control-Allow-Origin: $origin");
-// }
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
 
-// header("Access-Control-Allow-Credentials: true");
-// header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-// header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With,  Accept, Origin");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With,  Accept, Origin");
 // if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 //     header('HTTP/1.1 200 OK');
 //     exit();
