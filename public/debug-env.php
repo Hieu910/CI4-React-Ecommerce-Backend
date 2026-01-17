4fda6ef3f946dc3288089f204b586c753e5831d4
Hieu <handsomebanana910@gmail.com>
2026-01-17 07:10:48 +0700

    update

--- public/debug-env.php
+++ public/debug-env.php
@@ -1,6 +1,6 @@
 <?php
 echo "<pre>";
-echo "DB_HOST: " . ($_SERVER['DB_HOST'] ?? getenv('DB_HOST') ?? $_ENV['DB_HOST'] ?? 'NOT SET') . "\n";
-echo "DB_PORT: " . ($_SERVER['DB_PORT'] ??  getenv('DB_PORT') ?? $_ENV['DB_PORT'] ?? 'NOT SET') . "\n";
-echo "DB_USER: " . ($_SERVER['DB_USER'] ??   getenv('DB_USER') ?? $_ENV['DB_USER'] ?? 'NOT SET') . "\n";
+echo "DB_HOST: " . (getenv('DB_HOST') ?? $_ENV['DB_HOST'] ?? 'NOT SET') . "\n";
+echo "DB_PORT: " . (getenv('DB_PORT') ?? $_ENV['DB_PORT'] ?? 'NOT SET') . "\n";
+echo "DB_USER: " . (  getenv('DB_USER') ?? $_ENV['DB_USER'] ?? 'NOT SET') . "\n";
 echo "</pre>";
